72562ca2b581d2b2444e49326e98a4743236af23
MikeZhang <mike.zhang@everbridge.com>
2016-01-26 16:16:11 +0800

    add express

---
+++ server.js
@@ -0,0 +1,38 @@
+/**
+ * Created by mike on 16-1-26.
+ */
+var express        = require( 'express' );
+var http           = require( 'http' );
+var path           = require( 'path' );
+var static         = require( 'serve-static' );
+var fs             = require( 'fs');
+var app            = express();
+
+// all environments
+app.set( 'port', process.env.PORT || 3001 );
+app.engine( 'html', require('ejs').renderFile);
+
+app.set( 'views', path.join( __dirname, 'sample/mvc/pages' ));
+app.set( 'view engine', 'html' );
+app.use( static( path.join( __dirname, 'sample/mvc' )));
+
+app.get('/index',function(req, res, next) {
+    res.render('sample1', { title: 'Express' });
+});
+
+
+app.get(    '/getNixle',      function(req,res,next){
+    var data={
+        nixleId:'1',
+        nixleTitle:'123',
+        nixleMessage:'1234',
+        nixleCheck:[]
+    };
+    res.send({
+        message:"success",
+        data:data
+    });
+});
+http.createServer( app ).listen( app.get( 'port' ), function (){
+    console.log( 'Express server listening on port ' + app.get( 'port' ));
+});
